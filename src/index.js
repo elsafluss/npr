472db7edc903fd98bf3b960fcc0d0620bdceb02f
Elsa Fluss <elsafluss@gmail.com>
2021-06-06 14:12:42 -0600

    Add note

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ ReactDOM.render(
 // it would be cool if you could use CRA without the extras
 // but also with router maybe already added
 // that probably exists somewhere
+// but is it worth it?
