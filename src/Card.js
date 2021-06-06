cdd9c20861ed69f63afbecbd165abc8d2b6eebc0
Elsa Fluss <elsafluss@gmail.com>
2021-06-06 13:48:47 -0600

    Add note

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./Card.css"
 
 const Card = () => {
-  return <div className="card">This is the card</div>
+  return <div className="card">This is the card. Another generic component. Angular does this for you, you know.</div>
 }
 
 export default Card
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 const Header = () => {
     return(
-        <div className='header'>This is the header</div>
+        <div className='header'>This is the header, and this is how I set up my components, so that I can see if they're working before I do anything else.</div>
     )
 }
 
